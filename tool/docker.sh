94f01a1546b81efe0da401a3f892f4966487cd40
HeHongling <hank.sunday@gmail.com>
2016-03-18 14:26:32 +0800

    修改oc配置

--- tool/docker.sh
+++ tool/docker.sh
@@ -1,14 +1,15 @@
-docker build -t mongodb_aws .
+docker build -t maria .
 
 docker run -d -p 8000:8000 \
 	-e "ETCDENDPOINT=http://192.168.99.100:2379"  \
+	-e "ETCDUSER=asiainfoLDP" \
+	-e "ETCDPASSWORD=6ED9BA74-75FD-4D1B-8916-842CB936AC1A" \
 	-e "BROKERPORT=8000"	 \
-	-e "MONGOURL=54.222.155.67:27017"	 \
-	-e "MONGOADMINUSER=asiainfoLDP"  \
-	-e "MONGOADMINPASSWORD=6ED9BA74-75FD-4D1B-8916-842CB936AC1A"  \
-	-e "AWS_ACCESS_KEY_ID=XXX"
-	-e "AWS_SECRET_ACCESS_KEY=XXX"
-	--name mongodb_aws mongodb_aws
+	-e "MARATHONURL=http://10.1.235.96:5098"	 \
+	--name sbm maria
+
+
+
 
 
 docker run --name postgres -e POSTGRES_PASSWORD=C1BFACD6-E500-4257-B1BA-E7D369999C0F -p 5432:5432 -e POSTGRES_USER=asiainfoLDP -d postgres
--- tool/oc.sh
+++ tool/oc.sh
@@ -1,23 +1,17 @@
-oc login https://lab.asiainfodata.com:8443  -u hehl@asiainfo.com  -p 1857e645-c263-407d-b4e1-e82d15df8d6e --insecure-skip-tls-verify=true
+oc login https://lab.asiainfodata.com:8443  -u hehl@asiainfo.com  -p 1857e645-c263-407d-b4e1-e82d15df8d6e
 
-oc new-build https://github.com/asiainfoLDP/datafoundry_servicebroker_go.git
+oc new-build https://github.com/asiainfoLDP/datafoundry_servicebroker_marathon.git
 
-oc run servicebroker-mongo --image=172.30.32.106:5000/datafoundry-servicebroker/datafoundryservicebrokergo \
+oc run servicebroker-marathon --image=172.30.32.106:5000/datafoundry-servicebroker/datafoundryservicebrokermarathon  \
     --env  ETCDENDPOINT="http://54.222.175.239:2379"  \
     --env  ETCDUSER="asiainfoLDP" \
 	--env  ETCDPASSWORD="6ED9BA74-75FD-4D1B-8916-842CB936AC1A" \
     --env  BROKERPORT="8000"  \
     --env  MONGOURL="54.222.175.239:27017"  \
-    --env  MONGOADMINUSER="asiainfoLDP"   \
-    --env  MONGOADMINPASSWORD="6ED9BA74-75FD-4D1B-8916-842CB936AC1A"   \
-    --env  AWS_ACCESS_KEY_ID=AKIAO2SO52RKIE7BCSHA  \
-    --env  AWS_SECRET_ACCESS_KEY=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 \
-    --env  POSTGRESURL="54.222.175.239:5432" \
-    --env  POSTGRESUSER="asiainfoLDP" \
-    --env  POSTGRESADMINPASSWORD="C1BFACD6-E500-4257-B1BA-E7D369999C0F"
+    --env  MARATHONURL="http://54.222.170.101:5098"
 
-
-oc expose  svc servicebroker-mongo
+oc expose dc servicebroker-marathon --port 8000
+oc expose  svc servicebroker-marathon
 
 
 
